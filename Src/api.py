3e60816c3a1b2b5050bd5c54a47331c4c898561b
Tareq Perez <tareqx3@gmail.com>
2015-02-15 22:31:11 -0600

    heroku config

--- Src/api.py
+++ Src/api.py
@@ -31,4 +31,4 @@ def get_story():
 	return response, 200
 
 if __name__ == '__main__':
-    app.run(port=int(os.getenv("PORT","5000")))
+    app.run(host='0.0.0.0',port=int(os.getenv("PORT","5000")))
