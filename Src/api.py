bfb34f55cc803893de97e6ed983ce488d9880aae
Tareq Perez <tareqx3@gmail.com>
2015-02-16 00:35:40 -0600

    fix jsonify return

--- Src/api.py
+++ Src/api.py
@@ -27,7 +27,7 @@ def get_story():
 		storyline = BeautifulSoup(soup2[0].get_text())
 		storyline = storyline.findAll("p")
 		response["storyline"] = storyline
-	return jsonify(str(response)), 200
+	return str(response), 200
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=int(os.getenv("PORT","5000")))
