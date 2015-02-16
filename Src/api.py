3285f864b999bbc87e665fccf9baa45060337f26
Tareq Perez <tareqx3@gmail.com>
2015-02-15 22:40:59 -0600

    fix response format

--- Src/api.py
+++ Src/api.py
@@ -15,20 +15,19 @@ IMDB_RANDOM = "http://www.imdb.com/random/title"
 @app.route('/api/v1.0/story', methods=['GET'])
 def get_story():
 	resp = requests.get(IMDB_RANDOM)
-	response = ""
+	response = {}
 	soup = BeautifulSoup(resp.text)
 	description = soup.findAll("p", { "itemprop" : "description" })
 	response = {"description":description}
 	movieName = soup.findAll("span",{"itemprop": "name"})
 	response["Name"] = movieName[0].get_text()
 	soup2 = soup.findAll("div", { "itemprop" : "description" })
-
+	
 	if (soup2):
 		storyline = BeautifulSoup(soup2[0].get_text())
 		storyline = storyline.findAll("p")
 		response["storyline"] = storyline
-
-	return response, 200
+	return str(response), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=int(os.getenv("PORT","5000")))
+	app.run(host='0.0.0.0',port=int(os.getenv("PORT","5000")))
